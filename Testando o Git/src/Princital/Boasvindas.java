e555dd348ada365e78aef7947ac9aab8dd6728bc
Well <Well@DESKTOP-VJBRE17>
2021-12-28 14:11:10 -0300

    Tentativa numero 1 de Alteração

---
+++ Testando o Git/src/Princital/Boasvindas.java
@@ -0,0 +1,13 @@
+
+package Princital;
+public class Boasvindas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hellow Word");
+		System.out.println("Olá Mundo");
+
+	}
+
+}
